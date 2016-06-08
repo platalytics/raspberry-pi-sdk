53a9956a44cbdc83f7d4471d97e13f3829bebaaa
M.khalid <hafizmujadid00@gmail.com>
2016-06-08 14:25:02 +0500

    remove existing registered devices

--- src/video-streaming/start.sh
+++ src/video-streaming/start.sh
@@ -10,6 +10,7 @@ else
     height=$5
     url="http://$host:$port/$password/$width/$height/"
     echo $url
+    rm /dev/video*
     sudo uv4l --sched-rr --driver raspicam --auto-video_nr
     v4l2-ctl --set-fmt-video=width=$width,height=$height,pixelformat=H264 -d /dev/video0
     nohup dd if=/dev/video0 bs=512K | /usr/src/FFmpeg/ffmpeg -i - -f video4linux2 -f mpeg1video -b 800k -r 30 $url >/dev/null 2>&1 &
