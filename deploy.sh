9abe46b57c82324e323cdab98b833da425c97014
Sadaf_khan@bitbucket.com <sadaf@platalytics.com>
2016-06-13 10:59:07 +0500

    curl-notify file path correction in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,12 +17,12 @@ else
     chmod 755 ./copy.sh
     chmod 755 ./setup.sh
     chmod 755 ./setup-video.sh
-    chmod 755 ./root/core/ack/curl-notify.sh
+    chmod 755 ./core/ack/curl-notify.sh
 
     echo "starting..."
 
     #notifying frontend
-    ./root/core/ack/curl-notify.sh ${device_id} ${front_end_host} "1"
+    ./core/ack/curl-notify.sh ${device_id} ${front_end_host} "1"
 
     # monitoring script deployment
     ./copy.sh ${board_ip} ${board_username} ${board_password} ${ssh_port} ${core_path} /root/ 1>/dev/null
@@ -34,7 +34,7 @@ else
     ./copy.sh ${board_ip} ${board_username} ${board_password} ${ssh_port} ${libraries} /root/ 1>/dev/null
 
      #notifying frontend
-    ./root/core/ack/curl-notify.sh ${device_id} ${front_end_host} "2"
+    ./core/ack/curl-notify.sh ${device_id} ${front_end_host} "2"
 
     # setup
     ./setup.sh ${board_ip} ${board_username} ${board_password} ${device_id} ${ssh_port} ${front_end_host}
